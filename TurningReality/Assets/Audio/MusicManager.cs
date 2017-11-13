99c3df1d3c40ba7a3217397c1f9c6df80089c8ee
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2017-11-13 19:36:45 +0100

    Audio assets and managers

---
+++ TurningReality/Assets/Audio/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : SoundManager
+{
+    public override void Awake()
+    {
+        base.Awake();
+    }
+
+    public void Update()
+    {
+        PlayMusic();
+    }
+
+    private void PlayMusic()
+    {
+        if (!Source.isPlaying)
+        {
+            PlayRandomSoundClip();
+        }
+    }
+}
---
+++ TurningReality/Assets/Audio/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    List<AudioClip> audioClips;
+
+    protected AudioSource Source { get; private set; }
+    protected AudioClip RandomClip { get { return audioClips[Random.Range(0, audioClips.Count - 1)]; } }
+
+    public virtual void Awake()
+    {
+        Source = GetComponent<AudioSource>();
+    }
+
+    public void PlayRandomSoundClip()
+    {
+        Source.clip = RandomClip;
+        Source.Play();
+    }
+
+    public void PlayRandomOneShotEffect()
+    {
+        Source.PlayOneShot(RandomClip);
+    }
+}
