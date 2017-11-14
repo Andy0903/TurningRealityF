60df778bb9becd7f8aefc702513444868815523b
HampusS <supmahampus@gmail.com>
2017-11-15 00:07:45 +0100

    Added transparent material and a lerp position for the player when rotating

--- TurningReality/Assets/GameTools/RotationButton/Untouchables/ButtonManager.cs
+++ TurningReality/Assets/GameTools/RotationButton/Untouchables/ButtonManager.cs
@@ -9,6 +9,9 @@ public class ButtonManager : MonoBehaviour
     Transform worldTrans;
     GameObject[] buttons;
     GameObject player;
+    Vector3 LevitatePos;
+
+    bool playerInPosition = false;
 
     // Use this for initialization
     void Start()
@@ -30,15 +33,21 @@ public class ButtonManager : MonoBehaviour
     {
         if (currentTrig != null)
         {
-            currentTrig.Running(worldTrans);
-
-            if (currentTrig.Exit())
+            if (playerInPosition)
             {
-                SetKinematic(false, player);
-                player.transform.eulerAngles = new Vector3(0, player.transform.eulerAngles.y, 0);
-                player.GetComponent<Movement>().WorldIsRotating = false;
-                currentTrig = null;
+                currentTrig.Running(worldTrans);
+
+                if (currentTrig.Exit())
+                {
+                    SetKinematic(false, player);
+                    player.transform.eulerAngles = new Vector3(0, player.transform.eulerAngles.y, 0);
+                    player.GetComponent<Movement>().WorldIsRotating = false;
+                    currentTrig = null;
+                    playerInPosition = false;
+                }
             }
+            else
+                PlayerLevitates();
         }
         else
         {
@@ -51,14 +60,22 @@ public class ButtonManager : MonoBehaviour
                     {
                         player.GetComponent<Movement>().WorldIsRotating = true;
                         player.GetComponent<ObjectPusher>().ForceDropObject();
-
                         currentTrig = temp;
+                        LevitatePos = new Vector3(temp.transform.position.x, temp.transform.position.y + 2, temp.transform.position.z);
+
                         currentTrig.Enter();
                         SetKinematic(true, player);
                     }
                 }
             }
         }
+    }
+
+    private void PlayerLevitates()
+    {
+        player.transform.position = Vector3.Lerp(player.transform.position, LevitatePos, Time.deltaTime * 3);
+        if (Vector3.Distance(player.transform.position, LevitatePos) < 0.1)
+            playerInPosition = true;
     }
 
     public void SetKinematic(bool on, GameObject obj)
---
+++ TurningReality/Assets/Utilities/TransparantMaterial/UnRenderMe.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class works together with a transparent diffuse material
+public class UnRenderMe : MonoBehaviour
+{
+    Color startColor, currColor;
+    Transform cam;
+
+    void Start()
+    {
+        // Stores the initial color
+        startColor = gameObject.GetComponent<Renderer>().material.color;
+        cam = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+
+    void Update()
+    {
+        // get scalar to check angle between object's local x-axis (right) and world's z-axis (forward)
+        float dot = Vector3.Dot(cam.forward, transform.forward);
+
+        if (dot > 0.3)
+            currColor = new Color(startColor.r, startColor.g, startColor.b, Mathf.Lerp(currColor.a, 1 - dot, Time.deltaTime));
+        else
+            currColor = Color.Lerp(currColor, startColor, Time.deltaTime);
+
+        GetComponent<Renderer>().material.color = currColor;
+
+    }
+}
