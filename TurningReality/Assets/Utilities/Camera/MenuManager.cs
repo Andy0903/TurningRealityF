ff27f0b29957ca1327cd67841bb264255a039f9e
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2017-11-19 20:26:14 +0100

    Made pausemenu

---
+++ TurningReality/Assets/Utilities/Camera/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pauseMenu;
+
+    void LateUpdate()
+    {
+        if (Input.GetButtonDown("StartButton"))
+        {
+            if (Time.timeScale != 0)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+        }
+    }
+}
