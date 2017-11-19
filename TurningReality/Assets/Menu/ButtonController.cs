6c7e3ab7bcb5dd30498f9136ff4fb2ec5d5928f2
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2017-11-19 18:43:25 +0100

    make controller work in menu

--- TurningReality/Assets/Menu/ButtonController.cs
+++ TurningReality/Assets/Menu/ButtonController.cs
@@ -5,9 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
+    [SerializeField]
+    GameObject controlScheme;
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
-	
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else if (Input.GetButtonDown("Controls"))
+        {
+            controlScheme.SetActive(!controlScheme.activeSelf);
+        }
+        else if (Input.GetButtonDown("Cancel"))
+        {
+            Application.Quit();
+        }
+    }
+
 }
