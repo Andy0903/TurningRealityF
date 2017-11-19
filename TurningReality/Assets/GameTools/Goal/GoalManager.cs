509f132dd5366a843105fe5319fdb3d500ba4ba2
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2017-11-19 18:11:09 +0100

    Make goalmanager load level wrap around

--- TurningReality/Assets/GameTools/Goal/GoalManager.cs
+++ TurningReality/Assets/GameTools/Goal/GoalManager.cs
@@ -20,7 +20,7 @@ public class GoalManager : MonoBehaviour
             if (SceneManager.GetActiveScene().buildIndex + 1 <= SceneManager.sceneCountInBuildSettings)
             {
                 AudioManager.Instance.Play("Xylophone");
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
             }
         }
     }
