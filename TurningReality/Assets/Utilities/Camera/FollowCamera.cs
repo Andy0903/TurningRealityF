7138d930c129998d7c0fb3f27b0f58411ebd8e61
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2017-12-03 11:52:25 +0100

    Fixed camer lag (I introduced in last commit)

--- TurningReality/Assets/Utilities/Camera/FollowCamera.cs
+++ TurningReality/Assets/Utilities/Camera/FollowCamera.cs
@@ -100,7 +100,7 @@ public class FollowCamera : MonoBehaviour
         //}
     }
 
-    private void FixedUpdate()  //LateUpdate
+    private void LateUpdate() 
     {
         RotationInput();
         ZoomInput();
